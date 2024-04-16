db8e49b4a2da8fead3be1369f2519c98441ffb44
1yaochuan <97727024+1yaochuan@users.noreply.github.com>
2024-04-16 09:30:51 +0800

    Delete java/com/nkuvr directory

--- java/com/nkuvr/service/impl/ILabServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.nkuvr.service.impl;
-
-import com.nkuvr.service.ILabService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ILabServiceImpl implements ILabService {
-}
