19cc039c29efc426f999f2fff8fa0190436af451
1yaochuan <97727024+1yaochuan@users.noreply.github.com>
2024-04-16 09:28:54 +0800

    Add files via upload
    提交初始版本

---
+++ java/com/nkuvr/service/impl/ILabServiceImpl.java
@@ -0,0 +1,8 @@
+package com.nkuvr.service.impl;
+
+import com.nkuvr.service.ILabService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ILabServiceImpl implements ILabService {
+}
